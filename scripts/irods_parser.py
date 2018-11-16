390ef3c83b45ee4457307a0dc1208a8954f0bfb2
Ken Youens-Clark <kyclark@gmail.com>
2018-11-16 15:38:36 -0700

    reads stdin w/no args

--- scripts/irods_parser.py
+++ scripts/irods_parser.py
@@ -5,23 +5,26 @@ import sys
 import re
 
 args = sys.argv[1:]
+fh = None
 
-if len(args) != 1:
-    print('Usage: {} ils'.format(os.path.basename(sys.argv[0])))
-    sys.exit(1)
-
-ils = args[0]
-
-if not os.path.isfile(ils):
-    print('"{}" is not a file'.format(ils))
-    sys.exit(1)
+if len(args) == 0:
+    fh = sys.stdin
+elif len(args) == 1:
+    file = args[0]
+    if os.path.isfile(file):
+        fh = open(file, 'rt')
+    else:
+        print('"{}" is not a file'.format(file))
+        sys.exit(1)
+else:
+    print('Usage: {} <stdin|file>'.format(os.path.basename(args[0])))
 
 dir_re = re.compile('^(\/[^:]+)')
 coll_re = re.compile('^\s{2}C-\s+')
 file_re = re.compile('^\s{2}([^/]+)')
 
 dir = ''
-for line in open(ils):
+for line in fh:
     line = line.rstrip()
     dir_match = dir_re.search(line)
     coll_match = coll_re.search(line)
